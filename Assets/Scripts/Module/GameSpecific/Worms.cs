327f91240866db5bef72f510bfe72a8983e5ea84
JaGoTu <jagotu@gmail.com>
2024-11-30 20:21:08 +0100

    modules/gamespecific: add a module for game-specific functions

---
+++ Assets/Scripts/Module/GameSpecific/Worms.cs
@@ -0,0 +1,29 @@
+﻿using Nofun.Driver.Audio;
+using Nofun.Module.VMStream;
+using Nofun.Util.Logging;
+using Nofun.VM;
+using System;
+using System.IO;
+
+namespace Nofun.Module.GameSpecific
+{
+
+
+    [Module]
+    public partial class Worms
+    {
+        private VMSystem system;
+
+        public Worms(VMSystem system)
+        {
+            this.system = system;
+
+        }
+
+        [ModuleCall]
+        private void wormsSetFocusFunc()
+        {
+
+        }
+    }
+}
